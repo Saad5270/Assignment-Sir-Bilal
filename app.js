1a7a4c118430bab3161741a5dbf628dccf002d92
Saad <61311969+Saad5270@users.noreply.github.com>
2023-05-16 01:31:10 +0500

    added assignment 2

---
+++ app.js
@@ -0,0 +1,13 @@
+
+let taskDiv= document.getElementById('taskDiv');
+
+function asOnclick(){
+
+    if(taskDiv.style.display==='none'){
+        taskDiv.style.display= 'flex';
+    }
+    else{
+        taskDiv.style.display= 'none';
+
+    }
+}
